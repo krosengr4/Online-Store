849133248f2959f3816c9c870ea7742da4a1cc81
krosengr4 <rosenkev4@gmail.com>
2025-04-25 16:02:59 -0500

    Added filePath to products.csv, added empty homeScreen method, added method for the homeScreen messaging

--- Online-Store/src/main/java/Store.java
+++ Online-Store/src/main/java/Store.java
@@ -2,6 +2,20 @@ public class Store {
 
     public static void main(String[] args) {
 
+        String filePath = "Online-Store/src/main/resources/products.csv";
+        homeScreen();
+
     }
 
+    private static void homeScreen() {
+
+    }
+
+    public static int homeScreenMessaging() {
+        System.out.println("_____WELCOME TO THE ONLINE STORE_____");
+        return 0;
+    }
+
+
 }
+
