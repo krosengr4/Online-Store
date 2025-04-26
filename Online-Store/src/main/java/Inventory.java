1f70e44c4cadbae59fad25bde7c34e03ad294a94
krosengr4 <rosenkev4@gmail.com>
2025-04-26 03:28:20 -0500

    Completed method to display inventory menu. Started method to search for a product.

--- Online-Store/src/main/java/Inventory.java
+++ Online-Store/src/main/java/Inventory.java
@@ -1,7 +1,32 @@
 public class Inventory {
 
     public static void displayInventoryMenu() {
-        System.out.println("Displays the inventory menu");
+
+        int userAction;
+
+        while(true) {
+            System.out.println("\n\t---INVENTORY---");
+            System.out.println("OPTIONS: \n1 - Search For Product \n2 - Add Product to Cart \n3 - Return to Home Page");
+            userAction = Integer.parseInt(Utils.getUserInput("Enter the number of what you'd like to do: "));
+
+            if (userAction == 1) {
+                System.out.println("Enter product name.");
+                String userSearch = Utils.getUserInput("Search: ");
+                searchProduct(userSearch);
+            } else if (userAction == 2) {
+                System.out.println("Add a product to cart");
+                //! Create method in Cart.java class and add here!!!!!!!!!!!!!!!
+            } else if (userAction == 3) {
+                break;
+            } else {
+                System.err.println("ERROR! Please enter a number between 1 and 3!");
+            }
+        }
+    }
+
+    public static void searchProduct(String userSearch) {
+        System.out.println("Searching for: " + userSearch);
+
     }
 
 }
--- Online-Store/src/main/java/Main.java
+++ Online-Store/src/main/java/Main.java
@@ -4,23 +4,27 @@ public class Main {
         System.out.println("_____WELCOME TO THE ONLINE STORE_____");
         String filePath = "Online-Store/src/main/resources/products.csv";
 
-        int userHSAction = getHomeScreenAction();
+        int userHSAction;
+
+
+        userHSAction = getHomeScreenAction();
 
         switch (userHSAction) {
             case 1:
                 Inventory.displayInventoryMenu();
-                break;
             case 2:
                 Cart.displayCartMenu();
-                break;
             case 3:
                 System.out.println("Thank you for using the Online Store! \n\tPlease come again soon! :)");
-
+                break;
+            default:
+                System.err.println("ERROR: Please choose a number between 1 and 3!");
         }
 
     }
 
     public static int getHomeScreenAction() {
+        System.out.println("\n\t---HOME PAGE---");
         //Get number user inputs and return it
         System.out.println("1 - Inventory \n2 - Cart \n3 - Exit");
         int userAction = Integer.parseInt(Utils.getUserInput("Please enter the number of where you'd like to go: "));
