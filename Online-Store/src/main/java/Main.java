8614fe0fae4ab7f9cea6202d31647dd292b48097
krosengr4 <rosenkev4@gmail.com>
2025-04-26 02:31:50 -0500

    Created new class for the Cart and inventory

---
+++ Online-Store/src/main/java/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("_____WELCOME TO THE ONLINE STORE_____");
+        String filePath = "Online-Store/src/main/resources/products.csv";
+
+    }
+
+    public static int getHomeScreenAction() {
+        return 0;
+    }
+
+
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Online-Store
+# Online-Main
