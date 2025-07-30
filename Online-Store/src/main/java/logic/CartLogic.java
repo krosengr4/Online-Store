1eabad6d960d66feea5dd307c50c5607d51858bb
krosengr4 <rosenkev4@gmail.com>
2025-07-30 01:39:04 -0500

    Created method to process user cart screen option plus skeleton methods for each choice

--- Online-Store/src/main/java/logic/CartLogic.java
+++ Online-Store/src/main/java/logic/CartLogic.java
@@ -12,7 +12,25 @@ public class CartLogic {
 		while(ifContinue) {
 			int userChoice = ui.displayInventory();
 
+			switch(userChoice) {
+				case 1 -> displayCart();
+				case 2 -> removeItem();
+				case 3 -> checkout();
+				case 0 -> ifContinue = false;
+			}
 		}
+	}
+
+	private static void displayCart() {
+
+	}
+
+	private static void removeItem() {
+
+	}
+
+	private static void checkout() {
+
 	}
 
 }
