98a6d35bbbe4ba2c67f640563045eac0796bc922
krosengr4 <rosenkev4@gmail.com>
2025-07-29 19:07:53 -0500

    Updated the Utils class and added display home screen method in UserInterface

--- Online-Store/src/main/java/UserInterface.java
+++ Online-Store/src/main/java/UserInterface.java
@@ -1,2 +1,15 @@
 public class UserInterface {
+
+	public int displayHomePage() {
+		System.out.println("\n\t-----HOME PAGE-----");
+		System.out.println("""
+					---OPTIONS---
+				1 - Go to Inventory Screen
+				2 - Go to Cart Screen
+				0 - Exit
+				""");
+
+		return Utils.getUserInputIntMinMax("Enter here:\n", 0, 2);
+	}
+
 }
