3b8440fc5681169c97c80d070a3aacd4f7e0814c
krosengr4 <rosenkev4@gmail.com>
2025-07-30 02:15:06 -0500

    Created class to configure the database

--- Online-Store/src/main/java/config/DatabaseConfig.java
+++ Online-Store/src/main/java/config/DatabaseConfig.java
@@ -1,4 +1,16 @@
 package config;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+
 public class DatabaseConfig {
+
+	private static final BasicDataSource basicDataSource = new BasicDataSource();
+
+	private static BasicDataSource setConnection() {
+		basicDataSource.setUrl("jdbc:mysql://localhost:3306/online_store");
+		basicDataSource.setUsername("root");
+		basicDataSource.setPassword(System.getenv("SQL_PASSWORD"));
+		return basicDataSource;
+	}
+
 }
