a1739f45a7ed2b0a690415a59307cb34e161e876
Kevin Rosengren <rosenkev4@gmail.com>
2025-07-30 16:02:40 -0500

    Coded Order model class

--- Online-Store/src/main/java/models/Order.java
+++ Online-Store/src/main/java/models/Order.java
@@ -1,4 +1,74 @@
 package models;
 
+import java.time.LocalDateTime;
+
 public class Order {
+
+	int orderId;
+	LocalDateTime dateTime;
+	String customerName;
+	String address;
+	String state;
+	String city;
+
+	public Order() {}
+
+	public Order(int orderId, LocalDateTime dateTime, String customerName, String address, String state, String city) {
+		this.orderId = orderId;
+		this.dateTime = dateTime;
+		this.customerName = customerName;
+		this.address = address;
+		this.state = state;
+		this.city = city;
+	}
+
+	//region Setters and Getters
+	public int getOrderId() {
+		return orderId;
+	}
+
+	public void setOrderId(int orderId) {
+		this.orderId = orderId;
+	}
+
+	public LocalDateTime getDateTime() {
+		return dateTime;
+	}
+
+	public void setDateTime(LocalDateTime dateTime) {
+		this.dateTime = dateTime;
+	}
+
+	public String getCustomerName() {
+		return customerName;
+	}
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+	//endregion
 }
