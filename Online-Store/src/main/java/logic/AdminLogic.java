d90cf56f6ac76c4fd994876f8485b9ad328c7ee3
krosengr4 <rosenkev4@gmail.com>
2025-07-30 01:51:04 -0500

    Added method to process the admin screen with skeleton methods for each choice

--- Online-Store/src/main/java/logic/AdminLogic.java
+++ Online-Store/src/main/java/logic/AdminLogic.java
@@ -1,4 +1,36 @@
 package logic;
 
+import ui.UserInterface;
+
 public class AdminLogic {
+
+	static UserInterface ui = new UserInterface();
+
+	public static void processAdminScreen() {
+		boolean ifContinue = true;
+
+		while(ifContinue) {
+			int adminChoice = ui.displayAdminScreen();
+
+			switch(adminChoice) {
+				case 1 -> addProduct();
+				case 2 -> updateProduct();
+				case 3 -> deleteProduct();
+				case 4 -> addCategory();
+				case 5 -> deleteCategory();
+				case 0 -> ifContinue = false;
+			}
+		}
+	}
+
+	private static void addProduct() {}
+
+	private static void updateProduct() {}
+
+	private static void deleteProduct() {}
+
+	private static void addCategory() {}
+
+	private static void deleteCategory() {}
+
 }
